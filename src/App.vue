651636dccf6a1b3573fe1cdb87ba7e7cd89f30df
liuyang <451629802@qq.com>
2021-03-15 08:24:27 +0800

    修改head组件

--- src/App.vue
+++ src/App.vue
@@ -2,23 +2,7 @@
   <div id="app">
     <div class="admin-body">
       <!-- 头部操作区 -->
-      <div class="admin-header" :style="{
-        'padding' : `0 ${menuSetting.viewMargin}`,
-        'width' : `calc(100% - ${isCollapse ? menuSetting.smallWidth : menuSetting.width})`}">
-        <ul class="admin-header-left flex align-center justify-space-between">
-          <li>
-            <i :class="{
-              'el-icon-s-fold cursor hover-cblue' : !isCollapse,
-              'el-icon-s-unfold cursor hover-cblue' : isCollapse
-            }" @click="handleMenu"></i>
-          </li>
-          <li><i class="el-icon-refresh-right cursor"></i></li>
-        </ul>
-        <ul class="admin-header-right flex align-center justify-space-between">
-          <li><i class="el-icon-s-fold cursor"></i></li>
-          <li><el-avatar size="large" src="https://cube.elemecdn.com/3/7c/3ea6beec64369c2642b92c6726f1epng.png"></el-avatar></li>
-        </ul>
-      </div>
+      <head-title />  
       <!-- tag标签区 -->
       <tags />
       <!-- 左侧导航区 -->
@@ -39,6 +23,7 @@
   import setting from './config/setting'
   import MenuList from './components/menu'
   import Tags from './components/tags/tag'
+  import HeadTitle from './components/header'
   import { mapGetters } from 'vuex'
   export default {
     name:'',
@@ -51,7 +36,8 @@
 
     components: { 
       MenuList,
-      Tags
+      Tags,
+      HeadTitle,
     },
 
     computed: {
@@ -75,10 +61,6 @@
     },
 
     methods: {
-      /* 展开与隐藏 */
-      handleMenu(){
-        this.$store.commit('config/CHANGE_ISCOLLAPSE', !this.isCollapse)
-      },
       
       // 点击tag面签的时候
       tagClick (data) {
---
+++ src/components/header/index.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="admin-header" :style="{
+    'padding' : `0 ${menuSetting.viewMargin}`,
+    'width' : `calc(100% - ${isCollapse ? menuSetting.smallWidth : menuSetting.width})`}">
+    <ul class="admin-header-left flex align-center justify-space-between">
+      <li>
+        <i :class="{
+          'el-icon-s-fold cursor hover-cblue' : !isCollapse,
+          'el-icon-s-unfold cursor hover-cblue' : isCollapse
+        }" @click="handleMenu"></i>
+      </li>
+      <li><i class="el-icon-refresh-right cursor"></i></li>
+    </ul>
+    <ul class="admin-header-right flex align-center justify-space-between">
+      <li><i class="el-icon-s-fold cursor"></i></li>
+      <li><el-avatar size="large" src="https://cube.elemecdn.com/3/7c/3ea6beec64369c2642b92c6726f1epng.png"></el-avatar></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  import setting from '@/config/setting'
+  import { mapGetters } from 'vuex'
+  export default {
+    name:'',
+    props:[''],
+    data () {
+      return {
+        menuSetting: setting,
+      };
+    },
+
+    components: {},
+
+    computed: {
+      ...mapGetters([
+        'isCollapse',
+        'menuList',
+        'keepAliveArray',
+        'tags',
+      ]),
+    },
+
+    beforeMount() {},
+
+    mounted() {},
+
+    methods: {
+      /* 展开与隐藏 */
+      handleMenu(){
+        this.$store.commit('config/CHANGE_ISCOLLAPSE', !this.isCollapse)
+      },
+    },
+
+    watch: {}
+
+  }
+
+</script>
+<style lang='less' scoped>
+
+</style>
